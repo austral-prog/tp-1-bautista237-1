5795406bc410fa57a26e5508338aa273a7a2c227
bautista237 <162723056+bautista237@users.noreply.github.com>
2025-08-08 12:17:04 -0300

    Update exercise_math.py
    .

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,17 @@
 def math():
     a = 57
     b = 7
+    suma = a + b
+    resta = a - b
+    producto = a * b
+    promedio = suma / 2
+    div_entera = a // b
+    div_resto = a % b
+    div_real = a/b
+    print(suma)
+    print(resta)
+    print(producto)
+    print(promedio)
+    print(div_entera)
+    print(div_resto)
+    print(div_real)
